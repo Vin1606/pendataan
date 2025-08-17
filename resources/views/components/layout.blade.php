f86e48732d8adc9d755a7c5af22ee491fc549f2c
Vin1606 <corneliuskevin770@gmail.com>
2025-08-18 01:43:17 +0700

    perubahan

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ $title }}</title>
 
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
@@ -19,7 +20,6 @@
     {{-- FAVICON --}}
     <link rel="icon" sizes="32x32" href="http://simweb5.tftgrup.com/assets/img/logo.png" type="image/png">
 </head>
-@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
