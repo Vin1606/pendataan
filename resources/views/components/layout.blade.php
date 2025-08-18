38852b6b1805b627b3b9f74e69fd3614b7fbddbc
Vin1606 <corneliuskevin770@gmail.com>
2025-08-18 21:07:25 +0700

    perubahan

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
-<html lang="en" class="h-full">
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <title>{{ $title }}</title>
 
     {{-- External Fonts & Libraries --}}
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
 
-    {{-- Vite Assets --}}
+    {{-- Tailwind CDN --}}
     <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- Vite Assets --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     {{-- Favicon --}}
     <link rel="icon" sizes="32x32"
         href="https://service.tftgrup.com/javax.faces.resource/images/TerangFajar-Logo.PNG.xhtml" type="image/png">
 
+    {{-- Alpine.js --}}
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="h-full">
     <div class="min-h-full">
-        <x-navbar></x-navbar>
-        <x-header :subtitle="$subtitle"></x-header>
+        <x-navbar />
+        <x-header :subtitle="$subtitle" />
+
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 {{ $slot }}
@@ -40,6 +44,7 @@
     {{-- Font Awesome Kit --}}
     <script src="https://kit.fontawesome.com/c2752fde40.js" crossorigin="anonymous"></script>
 
+    {{-- Prevent Zoom Gesture --}}
     <script>
         document.addEventListener('gesturestart', function(e) {
             e.preventDefault();
